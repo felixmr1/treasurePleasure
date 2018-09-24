b51617a30b5b0a2365dc9256802df2f41edc1638
Felix Rosén <felix.m.rosen@gmail.com>
2018-09-24 17:33:34 +0200

    Add failed test to see how travis output them

--- TreasurePleasure/app/src/test/java/ExampleUnitTest.java
+++ TreasurePleasure/app/src/test/java/ExampleUnitTest.java
@@ -10,6 +10,16 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+        assertEquals(1, 2 + 2);
+    }
+
+    @Test
+    public void multiplication_isCorrect() {
+        assertEquals(1, 2 * 1);
+    }
+
+    @Test
+    public void subtraction_isCorrect() {
+        assertEquals(1, 6 - 2);
     }
 }
