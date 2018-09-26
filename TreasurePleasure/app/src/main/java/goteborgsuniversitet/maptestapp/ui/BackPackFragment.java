25112fe43e53972d7ca5b5939f57167a5e0a9b4c
dafors <da_fors@hotmail.com>
2018-09-26 19:17:44 +0200

    add gem png's, make items in bp individually clickable

--- TreasurePleasure/app/src/main/java/goteborgsuniversitet/maptestapp/ui/BackPackFragment.java
+++ TreasurePleasure/app/src/main/java/goteborgsuniversitet/maptestapp/ui/BackPackFragment.java
@@ -15,7 +15,7 @@ import goteborgsuniversitet.maptestapp.R;
 
 //Fragments must be embedded in a host activity
 
-public class BackPackFragment extends Fragment {
+public class BackPackFragment extends Fragment implements View.OnClickListener {
 
     //make reference to interface in order to check that the containing activity implements given interface
     CallBackMethodsInterface callBackMethodsInterface;
@@ -49,7 +49,8 @@ public class BackPackFragment extends Fragment {
         //inflate Android-Me fragment layout
         View rootView = inflater.inflate(R.layout.fragment_backpack, container, false);
 
-        //Get reference to the TextView in the fragment_backpack xml layout file
+        //Get reference to the imageView in the fragment_backpack xml layout file
+        /*
         ImageView clickImageView = (ImageView) rootView.findViewById(R.id.imageView11);
         clickImageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,14 +58,59 @@ public class BackPackFragment extends Fragment {
                 onClickBackpack(view);
             }
         });
+        */
 
-        //TODO populate backpack
+        //Setup Buttons
+        ImageView clickImageView11 = (ImageView) rootView.findViewById(R.id.imageView11);
+        ImageView clickImageView12 = (ImageView) rootView.findViewById(R.id.imageView12);
+        ImageView clickImageView13 = (ImageView) rootView.findViewById(R.id.imageView13);
+        ImageView clickImageView21 = (ImageView) rootView.findViewById(R.id.imageView21);
+        ImageView clickImageView22 = (ImageView) rootView.findViewById(R.id.imageView22);
+        ImageView clickImageView23 = (ImageView) rootView.findViewById(R.id.imageView23);
+
+        clickImageView11.setOnClickListener(this);
+        clickImageView12.setOnClickListener(this);
+        clickImageView13.setOnClickListener(this);
+        clickImageView21.setOnClickListener(this);
+        clickImageView22.setOnClickListener(this);
+        clickImageView23.setOnClickListener(this);
+
+        //TODO dynamically populate backpack
 
         return  rootView;
     }
 
-    public void onClickBackpack(View view) {
-        callBackMethodsInterface.receivedCallBack( "backPackFragment" + " reporting in");
+    public void onClickBackpack(View view, String s) {
+        callBackMethodsInterface.receivedCallBack( "backPackFragment view #" + s + " reporting in");
     }
 
+
+    //this class extends onClickListener. click functionality is handled here
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageView11:
+                onClickBackpack(v, "1:1");
+                break;
+            case R.id.imageView12:
+                onClickBackpack(v, "1:2");
+                break;
+            case R.id.imageView13:
+                onClickBackpack(v, "1:3");
+                break;
+            case R.id.imageView21:
+                onClickBackpack(v, "2:1");
+                break;
+            case R.id.imageView22:
+                onClickBackpack(v, "2:2");
+                break;
+            case R.id.imageView23:
+                onClickBackpack(v, "2:3");
+                break;
+            default: onClickBackpack(v, "unexpected behaviour");
+                break;
+        }
+    }
 }
+
+
