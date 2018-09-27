4a410f7182a15350866f7d3ee17970ea022b5089
oskarlyr <oskar.lyrstrand@gmail.com>
2018-09-27 15:33:45 +0200

    Added function to pickup items and empty to treasure chest. Added exceptions to various functions.

--- TreasurePleasure/app/src/main/java/goteborgsuniversitet/maptestapp/core/Player.java
+++ TreasurePleasure/app/src/main/java/goteborgsuniversitet/maptestapp/core/Player.java
@@ -8,6 +8,10 @@ class Player {
 
     private String username;
     private Avatar avatar;
+    private BackPack backpack;
+    //private Chest chest;
+    //private UpgradeCenter upgrades;
+
 
     Player(String name, Avatar avatar){
         this.username = username;
@@ -27,10 +31,44 @@ class Player {
         Matcher m = p.matcher(username);
         boolean b = m.find();
         if(b){
-             // if only normal characters and numbers are allowed
+            throw new IllegalArgumentException("Could not set username: Contains special characters."); // if only normal characters and numbers are allowed
+
         } else {
             this.username = username;
         }
+    }
+
+    void addToBackpack(Item i){
+        /* check if close enough
+
+        if(backpack.isFull()) {
+            throw new IllegalException("Could not collect item: Backpack is full")
+        }
+
+        int a = Math.abs(i.location.getX() - this.location.getX());
+        int b = Math.abs(i.location.getY() - this.location.getY());
+
+        int distance = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
+
+        if(distance < TreasurePleasure.minimumDistance){
+            this.backpack.addItem(i);
+        } else {
+            throw new IllegalException("Could not collect item: Too far away.");
+        }
+
+         */
+    }
+
+    void emptyBackpackToChest() {
+        /*
+        if(backpack.isEmpty()){
+            throw new IllegalException("Could not empty backpack: No items in backpack")
+        } else {
+            while(backpack.items.size > 0) {
+                chest.addItem(backpack.items.remove(backpack.items.size() - 1))
+            }
+        }
+         */
     }
 
     void setAvatar(Avatar avatar) {
