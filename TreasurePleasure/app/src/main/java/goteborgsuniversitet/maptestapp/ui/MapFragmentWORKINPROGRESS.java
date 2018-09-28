228dc624e3f0ea723c2eb69deed1b3ae38be29dd
dafors <da_fors@hotmail.com>
2018-09-28 09:35:04 +0200

    add map fragment to main activity

--- TreasurePleasure/app/src/main/java/goteborgsuniversitet/maptestapp/ui/MapFragmentWORKINPROGRESS.java
+++ TreasurePleasure/app/src/main/java/goteborgsuniversitet/maptestapp/ui/MapFragmentWORKINPROGRESS.java
@@ -1,6 +1,11 @@
 package goteborgsuniversitet.maptestapp.ui;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,6 +19,8 @@ import goteborgsuniversitet.maptestapp.R;
 
 public class MapFragmentWORKINPROGRESS extends SupportMapFragment implements OnMapReadyCallback {
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
     private GoogleMap mMap;
 
     //Hardcoded locations
@@ -21,6 +28,10 @@ public class MapFragmentWORKINPROGRESS extends SupportMapFragment implements OnM
 
     //Markers
     private Marker treasureChest;
+
+
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,5 +59,54 @@ public class MapFragmentWORKINPROGRESS extends SupportMapFragment implements OnM
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.chest))
                 .draggable(true));
 
+        enableMyLocation();
     }
+
+    private void enableMyLocation() {
+        //check if user has granted permission to use fine location:
+/*
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            //permission already granted
+            mMap.setMyLocationEnabled(true);
+        } else {
+            // Show rationale and request permission.
+            Toast.makeText(this,"Location permission required", Toast.LENGTH_SHORT).show();
+            getLocationPermission();
+        }
+*/
+
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            Toast.makeText(getActivity(),"Please provide location permission for app to work properly", Toast.LENGTH_SHORT).show();
+            getLocationPermission();
+
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    private void getLocationPermission() {
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1 );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                //if request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission was granted
+                    if (mMap != null) { mMap.setMyLocationEnabled(true);}
+                } else {
+                    //permission denied.
+                    Toast.makeText(getActivity(),"Location permission required", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
 }
