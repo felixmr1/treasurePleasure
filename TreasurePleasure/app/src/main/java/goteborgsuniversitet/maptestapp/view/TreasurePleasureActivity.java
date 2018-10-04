e3f3ab52460525b89e6f5c392346756e52cc0985
oskarlyr <oskar.lyrstrand@gmail.com>
2018-10-04 11:39:54 +0200

    Some changes to TPActivity

---
+++ TreasurePleasure/app/src/main/java/goteborgsuniversitet/maptestapp/view/TreasurePleasureActivity.java
@@ -0,0 +1,52 @@
+package goteborgsuniversitet.maptestapp.view;
+
+
+import android.os.Bundle;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import goteborgsuniversitet.maptestapp.R;
+
+import goteborgsuniversitet.maptestapp.presenter.TreasurePleasurePresenter;
+
+public class TreasurePleasureActivity extends AppCompatActivity implements TreasurePleasureView {
+
+    private View MapView;
+    TreasurePleasurePresenter presenter = new TreasurePleasurePresenter(this);
+    Button showBackPackButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showBackPackButton = findViewById(R.id.backpackButton);
+        presenter.onCreate();
+    }
+
+
+    @Override
+    public void showScoreboard() {
+
+    }
+
+    @Override
+    public void closeScoreboard() {
+
+    }
+
+    @Override
+    public void showSettings() {
+
+    }
+
+    @Override
+    public void closeSettings() {
+
+    }
+}
