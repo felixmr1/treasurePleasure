87fb724fc1f4cc838fff9b3dbb679ecac4c77a04
oskarlyr <oskar.lyrstrand@gmail.com>
2018-10-07 16:08:27 +0200

    Changes to collecible item

--- TreasurePleasure/app/src/main/java/treasure/pleasure/model/CollectableItem.java
+++ TreasurePleasure/app/src/main/java/treasure/pleasure/model/CollectableItem.java
@@ -4,39 +4,66 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 class CollectableItem {
-  private int maxItems;
-  private HashMap<Location, Item> collectables;
+  private int nrCollectibles;
+  private HashMap<Location, Item> collectibles;
   private ArrayList<Item> availableItems;
+  private ArrayList<Location> mapConstraint;
 
-  CollectableItem(int maxItems, ArrayList<Item> availableItems){
+  /**
+   * Creates an instance of collectible items, only one of these exist for each game.
+   * @param nrCollectibles Number of collectibles on the map
+   * @param availableItems All possible items that can be created
+   * @param mapConstraint The area of the map which all collectible items must be within
+   */
+
+  CollectableItem(int nrCollectibles, ArrayList<Item> availableItems, ArrayList<Location> mapConstraint){
     this.availableItems = availableItems;
-    this.maxItems = maxItems;
-    collectables = new HashMap<>();
+    this.nrCollectibles = nrCollectibles;
+    this.collectibles = new HashMap<>();
+    this.mapConstraint = mapConstraint;
 
-    for (int i = 0; i < maxItems; i++) {
+    for (int i = 0; i < nrCollectibles; i++) {
       this.spawnRandomItem();
     }
   }
 
+  /**
+   * Spawns a random item within the current map
+   */
   void spawnRandomItem() {
-    Location locationHandler = createUniqueLocation();
-    Item item = createRandomItem();
+    Location loc = createUniqueLocation();
+    Item collectible = createRandomItem();
 
-    addItem(locationHandler, item);
+    addItem(loc, collectible);
   }
 
-  private void addItem(Location location, Item item){
+  /**
+   * Adds an item
+   * @param loc
+   * @param item
+   */
+  private void addItem(Location loc, Item item){
 
   }
 
   Item createRandomItem() {
-    int random = (int) (Math.random()* availableItems.size());
+    int random = (int) (Math.random() * availableItems.size());
     return availableItems.get(random);
   }
 
+  /**
+   * Creates a location within mapConstraint that is TODO unique
+   * @return Location
+   */
   Location createUniqueLocation() {
+    Location northWest = mapConstraint.get(0);
+    Location southEast = mapConstraint.get(1);
+    double mapWidth = southEast.getLongitude() - northWest.getLongitude();
+    double mapHeight = northWest.getLatitude() - southEast.getLatitude();
+    double randX = northWest.getLongitude() + Math.random() * mapWidth;
+    double randY = northWest.getLatitude() + Math.random() * mapHeight;
 
-    return new Location();
+    return new Location(randX, randY);
   }
 
   /**
