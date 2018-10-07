b976ee9cafeea0496e4381e2d574521262fb6f97
Jesper Naarttijärvi <jesper.naarttijarvi@hotmail.se>
2018-10-07 16:06:18 +0200

    Refactored tests so that we can test package private classes

--- TreasurePleasure/app/src/main/java/goteborgsuniversitet/maptestapp/view/MainActivity.java
+++ TreasurePleasure/app/src/main/java/goteborgsuniversitet/maptestapp/view/MainActivity.java
@@ -8,8 +8,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-import treasure.pleasure.R;
-import treasure.pleasure.view.BackpackRecyclerViewFragment;
+import treasure.treasure.pleasure.R;
+import treasure.treasure.pleasure.view.BackpackRecyclerViewFragment;
 
 */
 /**
