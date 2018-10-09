d49b92fa80b59f3daa40dfb33f27be13b96d3512
John Lindström Gidskehaug <johnlindstrom92@hotmail.com>
2018-10-09 16:54:47 +0200

    fixed some merge errors that occured when i merged master into johndev

--- TreasurePleasure/app/src/main/java/treasure/pleasure/presenter/TreasurePleasurePresenter.java
+++ TreasurePleasure/app/src/main/java/treasure/pleasure/presenter/TreasurePleasurePresenter.java
@@ -1,17 +1,23 @@
 package treasure.pleasure.presenter;
 
 import com.google.android.gms.maps.model.LatLng;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+
+import treasure.pleasure.data.AndroidImageAssets;
+import treasure.pleasure.data.Tuple;
 import treasure.pleasure.model.Avatar;
+import treasure.pleasure.model.ItemType;
 import treasure.pleasure.model.TreasurePleasure;
-import treasure.pleasure.view.BackpackRecyclerViewFragment;
+import treasure.pleasure.view.BackpackFragment;
 import treasure.pleasure.view.TreasurePleasureView;
 
 public class TreasurePleasurePresenter {
 
   private TreasurePleasureView view;
-  private BackpackRecyclerViewFragment backpackView;
+  private BackpackFragment backpackView;
   private TreasurePleasure model;
   private GameMapPresenter gameMapPresenter;
 
@@ -52,14 +58,43 @@ public class TreasurePleasurePresenter {
   }
 
   //----------------------backpack stuff------------------------------------
-  public void setBackpackView (BackpackRecyclerViewFragment view){
+  public void setBackpackView (BackpackFragment view){
     this.backpackView = view;
   }
   //Retrieves arrayList from model representing the backpack content.
   //Passes list to backpackRecyclerView to be displayed.
   public void retrieveAndDisplayContent() {
-    backpackView.displayContent(model.getBackpackContents());
+    backpackView.displayContent(backPackItemsToDisplay());
   }
+
+
+
+
+  private List<Tuple<Integer, String>> backPackItemsToDisplay() {
+    List<Tuple<Integer, String>> dataToView = new ArrayList();
+
+    for (Tuple<ItemType, Double> tuple : model.getBackPackContent()) {
+      ItemType itemType = tuple.getField1();
+      String score = tuple.getField2().toString();
+
+      dataToView.add(new Tuple<>(getImages(itemType),score));
+
+    }
+
+    return dataToView;
+  }
+
+
+
+  private Integer getImages(ItemType type){
+
+    return AndroidImageAssets.getImages().get(type);
+
+  }
+
+
+
+
 
   //public void detachView() {
   //  mView=null;
