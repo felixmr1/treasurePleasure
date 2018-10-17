24a74c21719e774349ddfbd8c2a4678ab2ebfa33
John Lindström Gidskehaug <johnlindstrom92@hotmail.com>
2018-10-17 13:30:31 +0200

    added more logo stuff, added signupactivity

---
+++ TreasurePleasure/app/src/main/java/treasure/pleasure/view/SignupActivity.java
@@ -0,0 +1,35 @@
+package treasure.pleasure.view;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import treasure.pleasure.R;
+import treasure.pleasure.presenter.TreasurePleasurePresenter;
+
+
+public class SignupActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+    private static final String TAG = "SignupActivity";
+
+
+
+
+
+
+
+
+}
